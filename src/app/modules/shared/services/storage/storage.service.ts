b411b6d3a96a5cb2e2cad0e6f7b34d9143e0a72d
DevKike <alvaronarvaez151@gmail.com>
2024-11-03 02:12:19 -0500

    Feat: add storage service with upload and get url from the uploaded file logic

--- src/app/modules/shared/services/storage/storage.service.ts
+++ src/app/modules/shared/services/storage/storage.service.ts
@@ -1,7 +1,26 @@
 import { Injectable } from '@angular/core';
+import { AngularFireStorage } from '@angular/fire/compat/storage';
+import { lastValueFrom } from 'rxjs';
 @Injectable({
   providedIn: 'root',
 })
 export class StorageService {
-  constructor() {}
+  constructor(private readonly _ngFireStorageSrv: AngularFireStorage) {}
+
+  public async upload(filePath: string, file: any): Promise<void> {
+    try {
+      await this._ngFireStorageSrv.upload(filePath, file);
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  public async getUrl(filePath: string): Promise<string> {
+    try {
+      const pathRef = this._ngFireStorageSrv.ref(filePath);
+      return await lastValueFrom(pathRef.getDownloadURL());
+    } catch (error) {
+      throw error;
+    }
+  }
 }
