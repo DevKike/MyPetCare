dd0b00108cac554c54466adc6e8f02eb1cacb492
DevKike <alvaronarvaez151@gmail.com>
2024-11-04 01:26:27 -0500

    Feat: add modals components

---
+++ src/app/modules/shared/components/card-modal/card-modal.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-card-modal',
+  templateUrl: './card-modal.component.html',
+  styleUrls: ['./card-modal.component.scss'],
+})
+export class CardModalComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
---
+++ src/app/modules/shared/components/sheet-modal/sheet-modal.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sheet-modal',
+  templateUrl: './sheet-modal.component.html',
+  styleUrls: ['./sheet-modal.component.scss'],
+})
+export class SheetModalComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
