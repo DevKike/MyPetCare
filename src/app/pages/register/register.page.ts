9cec778a7918c17863a4d41293e9da097fb59921
DevKike <alvaronarvaez151@gmail.com>
2024-11-07 01:22:38 -0500

    Refactor: update logic using the form

--- src/app/pages/register/register.page.ts
+++ src/app/pages/register/register.page.ts
@@ -31,27 +31,28 @@ export class RegisterPage implements OnInit {
 
   ngOnInit() {
     this.initForm();
-
   }
 
   protected async doRegister() {
     try {
-      const authUser: IAuthUser = {
-        email: 'alvarito@gmail.com',
-        password: 'alvaro123',
-      };
 
-      const newUser: ICreateUser = {
-        name: 'John',
-        lastName: 'John',
-        age: 23,
-        phoneNumber: 3013259938,
-      };
+      const authUser: IAuthUser = {
+        email: this.registerForm.value.email,
+        password: this.registerForm.value.password
+      }
+
+      const userData: ICreateUser = {
+        name: this.registerForm.value.name,
+        lastName: this.registerForm.value.lastName,
+        age: this.registerForm.value.age,
+        phoneNumber: this.registerForm.value.phoneNumber,
+        imageUrl: this.registerForm.value.imageUrl
+      }
 
       const res = await this._authSrv.register(authUser.email, authUser.password);
       const userId = res.user?.uid;
 
-      await this._firestoreSrv.save(FirestoreCollection.USERS, newUser, userId);
+      await this._firestoreSrv.save(FirestoreCollection.USERS, userData, userId);
     } catch (error) {
       throw error;
     }
@@ -62,16 +63,16 @@ export class RegisterPage implements OnInit {
     this.password = new FormControl('', [Validators.required]);
     this.name = new FormControl('', [Validators.required]);
     this.lastName = new FormControl('', [Validators.required]);
-    /* this.age = new FormControl('', [Validators.required, Validators.min(0)]);
-    this.phoneNumber = new FormControl('', [Validators.required]); */
+    this.age = new FormControl('', [Validators.required, Validators.min(0)]);
+    this.phoneNumber = new FormControl('', [Validators.required]);
 
     this.registerForm = new FormGroup({
       email: this.email,
       password: this.password,
       name: this.name,
       lastName: this.lastName,
-     /*  age: this.age,
-      phone_number: this.phoneNumber, */
+      age: this.age,
+      phone_number: this.phoneNumber,
     });
   }
 }
