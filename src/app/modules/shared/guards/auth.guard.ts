cc664f690495683280d940eb9f725c211feeb414
DevKike <alvaronarvaez151@gmail.com>
2024-11-10 15:36:49 -0500

    Feat: add auth

---
+++ src/app/modules/shared/guards/auth.guard.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { AuthService } from '../services/auth/auth.service';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthGuard implements CanActivate {
+  constructor(
+    private readonly _authSrv: AuthService,
+    private readonly _router: Router
+  ) {}
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ):
+    | Observable<boolean | UrlTree>
+    | Promise<boolean | UrlTree>
+    | boolean
+    | UrlTree {
+    return this._authSrv.isAuth().then((isAuth) => {
+      if (isAuth) {
+        return true;
+      } else {
+        this._router.navigate(['sign-in']);
+        return false;
+      }
+    });
+  }
+}
