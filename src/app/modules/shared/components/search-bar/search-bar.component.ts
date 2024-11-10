b6b60f720b435845755b54124dcb8d7212efc289
sahir ruiz <sahirruiz4@gmail.com>
2024-11-10 15:15:59 -0500

    Feat: pet array was added to display

--- src/app/modules/shared/components/search-bar/search-bar.component.ts
+++ src/app/modules/shared/components/search-bar/search-bar.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { IonModal } from '@ionic/angular';
 
 @Component({
   selector: 'app-search-bar',
@@ -6,22 +7,69 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search-bar.component.scss'],
 })
 export class SearchBarComponent implements OnInit {
-  private data = [];
+  @Input() modal!: IonModal;
+  @Output() breedSelected = new EventEmitter<string>();
+  private data = [
+    'Labrador Retriever',
+    'Pastor Alemán',
+    'Golden Retriever',
+    'Bulldog Francés',
+    'Beagle',
+    'Poodle',
+    'Rottweiler',
+    'Yorkshire Terrier',
+    'Boxer',
+    'Dálmata',
+    'Husky Siberiano',
+    'Border Collie',
+    'Chihuahua',
+    'Pug',
+    'Shih Tzu',
+    'Bulldog Inglés',
+    'Akita',
+    'Samoyedo',
+    'Cocker Spaniel',
+    'Doberman',
+    'Persa',
+    'Siamés',
+    'Maine Coon',
+    'Bengalí',
+    'Ragdoll',
+    'Sphynx',
+    'British Shorthair',
+    'Scottish Fold',
+    'Devon Rex',
+    'Birmano',
+    'Siberiano',
+    'Cornish Rex',
+    'Abisinio',
+    'Bombay',
+    'Tonkinés',
+    'Azul Ruso',
+    'Savannah',
+    'Angora Turco',
+    'Chartreux',
+    'Ocicat',
+  ];
 
   protected results = [...this.data];
 
   ngOnInit(): void {
-    // this.results = this.data.sort((a, b) => a.localeCompare(b));
+    this.results = this.data.sort((a, b) => a.localeCompare(b));
   }
 
   protected handleInput(event: any) {
-    /* const query = event.target.value.toLowerCase();
+    const query = event.target.value.toLowerCase();
     this.results = this.data
       .filter((d) => d.toLowerCase().includes(query))
-      .sort((a, b) => a.localeCompare(b)); */
+      .sort((a, b) => a.localeCompare(b));
   }
 
   protected handleItemClick(item: string) {
-    console.log(item);
+    this.breedSelected.emit(item); 
+    if (this.modal) {
+      this.modal.dismiss();
+    }
   }
 }
+
