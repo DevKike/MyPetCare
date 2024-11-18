90f1c932f0939d73bdb29d43b14bba721f21e0c6
Jared Trujillo <148577531+DevJaredt@users.noreply.github.com>
2024-11-17 23:10:30 -0500

    Feat: add new method

--- src/app/modules/shared/services/localStorage/local-storage.service.ts
+++ src/app/modules/shared/services/localStorage/local-storage.service.ts
@@ -5,7 +5,33 @@ import { Preferences } from '@capacitor/preferences';
   providedIn: 'root',
 })
 export class LocalStorageService {
-  constructor() {}
+  constructor() {
+  }
+
+  async requestFileUploadPermission(): Promise<boolean> {
+    const permissionGranted = await this.getPermission('fileUploadPermission');
+    
+    if (permissionGranted) {
+      return true;
+    }
+
+    const userConfirmed = confirm('¿Permites que esta aplicación suba archivos PDF?');
+    
+    if (userConfirmed) {
+      await this.setPermission('fileUploadPermission', true);
+      return true;
+    } else {
+      await this.setPermission('fileUploadPermission', false);
+      return false;
+    }
+  }
+
+  async getPermission(key: string): Promise<boolean> {
+    const { value } = await Preferences.get({ key });
+    return value === 'true';
+  }
+
+
 
   async setPermission(key: string, value: boolean): Promise<void> {
     await Preferences.set({
