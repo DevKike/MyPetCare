dbea79adaeada1bb1a6e83528b91467e4d36de3f
DevKike <alvaronarvaez151@gmail.com>
2024-11-18 19:48:18 -0500

    Refactor: change minor things

--- src/app/pages/vaccine/vaccine.module.ts
+++ src/app/pages/vaccine/vaccine.module.ts
@@ -1,8 +1,4 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-
-import { IonicModule } from '@ionic/angular';
 
 import { VaccinePageRoutingModule } from './vaccine-routing.module';
 
@@ -11,9 +7,6 @@ import { SharedModule } from 'src/app/modules/shared/shared.module';
 
 @NgModule({
   imports: [
-    CommonModule,
-    FormsModule,
-    IonicModule,
     VaccinePageRoutingModule,
     SharedModule
   ],
